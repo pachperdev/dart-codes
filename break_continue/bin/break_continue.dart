963cfa21019824204890d91217521f45c7767e0e
pachperdev <pachperdev@gmail.com>
2022-04-24 13:30:47 -0500

    24-02-2022

---
+++ break_continue/bin/break_continue.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  for (var i = 0; i < 5; i++) {
+    print(i);
+    continue;
+    print('estos son los numeros del 1 al 5');
+  }
+
+  print('----------');
+
+  for (var i = 0; i < 5; i++) {
+    print(i);
+    break;
+    print('estos son los numeros del 1 al 5');
+  }
+}
---
+++ break_continue/test/breack_continue_test.dart
@@ -0,0 +1,8 @@
+import 'package:breack_continue/breack_continue.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
