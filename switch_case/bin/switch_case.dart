69850cdd95b2a37135ea289f3dd68b7735fa0921
pachperdev <pachperdev@gmail.com>
2022-04-24 12:38:46 -0500

    caso switch

---
+++ switch_case/bin/switch_case.dart
@@ -0,0 +1,21 @@
+void main(List<String> args) {
+  int opcion;
+  opcion = 1;
+
+  switch (opcion) {
+    case 1:
+      print('caso 1');
+      break;
+
+    case 2:
+      print('caso 3');
+      break;
+
+    case 3:
+      print('caso 3');
+      break;
+
+    default:
+      print('ninguna opcion seleccionada');
+  }
+}
