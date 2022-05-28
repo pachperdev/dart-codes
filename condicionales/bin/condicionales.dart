377636756fe2a82d2e9fb335b6baff1c1efb33cc
pachperdev <pachperdev@gmail.com>
2022-05-28 08:39:55 -0500

    test condicionales

--- condicionales/bin/condicionales.dart
+++ condicionales/bin/condicionales.dart
@@ -4,31 +4,8 @@ de dato String
 
 La idea es que el programa sea capaz de reconocer cuando un correo es valido o NO valido
  */
-void main(List<String> args) {
-  String correo;
-  String mensaje;
 
-  correo = 'mauricio@email.com'; // variabele correo
-
-  print(correo.contains(
-      '@')); //--> retorna un false o true, segun se cumpla la condicion
-  print(correo.endsWith(
-      '.com')); //--> retorna un false o true, segun se cumpla la condicion
-
+bool correoValidar(String correo) {
   //esta pieza de condigo es una forma muy particular de un "if"
-  mensaje = correo.contains('@') && correo.endsWith('.com')
-      ? 'Este es un correo electronico valido \n'
-      : 'Este no es un correo valido \n';
-
-  final spliteed = correo.split('@'); //metodo split
-
-  //finalmnete imprimimos las variables de interes
-  print(mensaje);
-
-  print('El numero de caracteres que tiene el correo es: ${correo.length}\n');
-
-  print(
-      'De esta manera corto el mensaje, luego del caracter "@" con un split: ${spliteed[1]}\n');
-
-  print('el tamaño del vector es: ${spliteed[1]}\n');
+  return correo.contains('@') && correo.endsWith('.com') ? true : false;
 }
--- condicionales/test/condicionales_test.dart
+++ condicionales/test/condicionales_test.dart
@@ -1,8 +1,28 @@
 import 'package:condicionales/condicionales.dart';
 import 'package:test/test.dart';
 
+import '../bin/condicionales.dart';
+
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test(
+      'dado correo mauricio@gmail.com, cuando coreoValidar se llama, estonces debe retornar true',
+      () {
+    String correo = 'mauricio@gmail.com';
+    final correoValid = correoValidar(correo);
+    expect(correoValid, true);
+  });
+  test(
+      'dado un correo vacio, cuando coreoValidar se llama, estonces debe retornar false',
+      () {
+    String correo = '';
+    final correoValid = correoValidar(correo);
+    expect(correoValid, false);
+  });
+  test(
+      'dado un correo sin arroba, cuando coreoValidar se llama, estonces debe retornar false',
+      () {
+    String correo = 'hjdfnd.com';
+    final correoValid = correoValidar(correo);
+    expect(correoValid, false);
   });
 }
