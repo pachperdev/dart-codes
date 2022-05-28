1bfed637962e9d2aea46dd16b74e3c090cf85d85
pachperdev <pachperdev@gmail.com>
2022-05-28 09:23:01 -0500

    test list

--- listas/bin/listas.dart
+++ listas/bin/listas.dart
@@ -2,23 +2,26 @@
 En este ejemplo veremos que es una lista y como utilizarla
 lISTA: una lista es una coleccion de objetos de tipo tipada o incluso dinamica
 */
-void main(List<String> args) {
-  List<int> listaEnteros = [1, 2, 3, 4, 5];
-  listaEnteros.add(200);
-  listaEnteros.add(300); //Con este metodo agrego valores a la lista de enteros
-  print(listaEnteros);
 
+List listaEnteros(List<int> param) {
+  List<int> listEnteros = [1, 2, 3, 4, 5];
+  listEnteros.add(200);
+  listEnteros.add(300); //Con este metodo agrego valores a la lista de enteros
+  return listEnteros;
+}
+
+List listaString() {
   List<String> listaPaises = ['Mexico', 'Colombia', 'Argentina'];
   listaPaises.addAll([
     'peru',
     'Chile',
     'Venezuela'
   ]); //Con el metodo ".addAll" agrego una colecion a mi lista
-  print(listaPaises);
-  print(listaPaises[
-      1]); // De esta menera imprimo la posicion numero uno de la lista(Colombia)
+  return listaPaises;
+}
 
+dynamic listaDynamica() {
   List<dynamic> listaDinamica = ['primero'];
   listaDinamica.addAll(['hola', 1, 3, true, false, 'otro string']);
-  print(listaDinamica);
+  return listaDinamica;
 }
--- listas/test/listas_test.dart
+++ listas/test/listas_test.dart
@@ -1,8 +1,13 @@
 import 'package:listas/listas.dart';
 import 'package:test/test.dart';
+import '../bin/listas.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('dado una lista de [1, 2, 3, 4, 5, 200, 300], valido si es correcta',
+      () {
+    List<int> param = [1, 2, 3, 4, 5, 200, 300];
+    final listint = listaEnteros(param);
+    expect(listint, [1, 2, 3, 4, 5, 200, 300]);
+    //como valido si es tipo intero o no, dado cualquier entrada de enteros a la lista
   });
 }
